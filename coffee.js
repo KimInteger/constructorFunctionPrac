4905755374f8ff64139506d9c844f30a18dae371
KimInteger <kjs20151240@gmail.com>
2024-06-03 15:54:36 +0900

    constructor에서 set과 get을 사용하여 규격을 지정하고 get으로 가져왔다.

--- coffee.js
+++ coffee.js
@@ -5,7 +5,39 @@ class makeCoffee {
     this.sugar = sugar;
     this.milk = milk;
   }
+  // *************** 규격만들기 ****************
+
+  set wondoo(value) {
+    if(typeof(value) === "string"){
+      // * 관례적으로 이렇게 표기하고 있다.
+      // * _의 뜻은 무언가 세팅을 통과한 값을 언더바를 붙이는 관습이 있다.
+      // ? 규격에 맞지 않으면, 자체적으로 빼버림.
+      this._wondoo = value;
+    } else {
+      console.error("이 원두가 아니야!!!!!!!! wondoo는 오직.. 문자열이여야만 해...");
+    }
+  }
+
+  // * get은 객체의 키값에 접근하여 값을 변경해 줄 수 있구나.
+  get wondoo() {
+    return this._wondoo + '산';
+  }
+
+
+  set water(value) {
+    if(typeof(value) === 'number'){
+      this._water = value;
+    } else {
+      console.error('워러는 넘버여야만 해!!!')
+    }
+  }
+
+  get water() {
+    return this._water + 'ml';
+  }
 }
 
-let test = new makeCoffee('대전','300ml','5g','100ml');
+let test = new makeCoffee('대전',300,'5g','100ml');
 console.log(test);
+console.log(test.wondoo);
+console.log(test.water);
