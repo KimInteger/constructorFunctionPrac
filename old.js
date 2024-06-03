cb9ca8066b2dd18426cddedb1d42547ee6c5bdea
KimInteger <kjs20151240@gmail.com>
2024-06-03 15:36:46 +0900

    생성자함수를 사용하며 만듦

--- old.js
+++ old.js
@@ -3,5 +3,19 @@ function old(a, b) {
   this.second = b;
 }
 
-let test = new old(1,2);
-console.log(test);
+
+class newOld {
+  constructor(a,b){
+    this.first = a;
+    this.second = b;
+  }
+  add(){
+    return this.first + this.second;
+  }
+}
+
+let testC = new newOld(1,2);
+let testCAdd = testC.add();
+
+console.log(testC);
+console.log(testCAdd);
