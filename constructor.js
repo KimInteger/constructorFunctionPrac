8329e3106bceac83e92078799da04181677798df
KimInteger <kjs20151240@gmail.com>
2024-06-03 14:48:52 +0900

    this매서드를통해서 객체 생성

--- constructor.js
+++ constructor.js
@@ -1,1 +1,19 @@
-class 
+// 분류규격 라면볶음면 {
+//   재료들(라면종류, 라면물, 추가재료) {
+//     라면종류 : 라면종류;
+//     라면물 : 라면물;
+//     추가재료 : 추가재료;
+//   }
+// }
+
+class yakiRamen {
+  constructor(ramenName, ramenWater, addFood) {
+    this.ramenName = ramenName,
+    this.ramenWater =ramenWater,
+    this.addFood = addFood
+  }
+}
+
+let test = new yakiRamen("신라면 건면", "물 550ml", "계란,떡볶이 떡");
+
+console.log(test);
