9e21768aaf8142aa84cfd74a3cf99cef981d2379
KimInteger <kjs20151240@gmail.com>
2024-06-03 11:58:46 +0900

    함수 workThree를 선언하고 return값과 실행문 지정

--- work-3.js
+++ work-3.js
@@ -1,1 +1,13 @@
-console.log('work-3');
+function workThree(a, b) {
+  // * logic : 두개의 매개변수를 받아서 객체를 생성함.
+  // * logic : 각각의 변수명은 각각의 키와 값이 됨.
+
+  let local = {};
+  local[a] = a;
+  local[b] = b;
+
+  return local;
+};
+
+let workmen = workThree("흐아","하!");
+console.log(workmen);
