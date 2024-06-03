25a099a205d3667e540f2a7d0b08227a86aed444
KimInteger <kjs20151240@gmail.com>
2024-06-03 11:49:47 +0900

    매개변수로 string타입만 받을 수 있도록 if문을 조성

--- work-1.js
+++ work-1.js
@@ -5,14 +5,20 @@
 function workOne(a, b) {
 // * logic : 매개변수 두개를 받은 다음에 
 // * logic : return은 객체였음 좋겠다.
-  let localVariable = {};
-  localVariable[a] = a;
-  localVariable[b] = b;
 
-  return localVariable;
+// * condition : 매개변수가 모두 문자열이여만 한다.
+  if(typeof(a) === 'string' && typeof(b) === 'string'){
+    let localVariable = {};
+    localVariable[a] = a;
+    localVariable[b] = b;
+
+    return localVariable;
+  } else {
+    console.error('좋은말할 때 문자열로 해라.');
+  }
 }
 
-let obj = workOne("김정수","배고픔");
+let obj = workOne("갓겜","망겜");
 
 console.log(obj);
 console.log(typeof(obj));
