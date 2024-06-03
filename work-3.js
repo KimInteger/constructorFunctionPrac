597975feca6aad32d6c3ccca83baf82493d6246a
KimInteger <kjs20151240@gmail.com>
2024-06-03 12:05:02 +0900

    함수 능력 바꿈

--- work-3.js
+++ work-3.js
@@ -1,13 +1,13 @@
-function workThree(a, b) {
+function workThree(index, name) {
   // * logic : 두개의 매개변수를 받아서 객체를 생성함.
   // * logic : 각각의 변수명은 각각의 키와 값이 됨.
 
-  let local = {};
-  local[a] = a;
-  local[b] = b;
-
-  return local;
+  let local = {
+    number : index+1,
+    name : name
+  };
+  return local
 };
 
-let workmen = workThree("흐아","하!");
-console.log(workmen);
+let test = workThree(0,"JACK MET");
+console.log(test);
