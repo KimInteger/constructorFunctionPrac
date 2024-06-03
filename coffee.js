eb77ef02aabe5760e6738d2cfffc3493ee019bdb
KimInteger <kjs20151240@gmail.com>
2024-06-03 15:40:41 +0900

    생성자함수로 객체가 생성되는 것을 확인함

--- coffee.js
+++ coffee.js
@@ -1,5 +1,11 @@
-class coffee {
-  constructor() {
-    
+class makeCoffee {
+  constructor(wondoo, water, sugar, milk) {
+    this.wondoo = wondoo;
+    this.water = water;
+    this.sugar = sugar;
+    this.milk = milk;
   }
 }
+
+let test = new makeCoffee('대전','300ml','5g','100ml');
+console.log(test);
